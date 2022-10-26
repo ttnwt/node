04bf9d094d95f404d1df65834c3680020d1800fe
ttnwt <103345581+ttnwt@users.noreply.github.com>
2022-10-25 23:51:33 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,11 @@
 print("Hello World")
-def jenkins = Jenkins.instance
-def computers = jenkins.computers
-computers.each {
-   println "${it.displayName} ${it.hostName}"
-}
+def node_exists(self, name):
+        '''
+        :param name: Name of Jenkins node, ``str``
+        :returns: ``True`` if Jenkins node exists
+        '''
+        try:
+            self.get_node_info(name)
+            return True
+        except JenkinsException:
+            return False
