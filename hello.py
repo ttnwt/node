9e68f7259dfc0eb96630ba7b7ef2b077e2af418e
ttnwt <103345581+ttnwt@users.noreply.github.com>
2022-10-25 23:53:54 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,11 +1,1 @@
 print("Hello World")
-def node_exists(self, name):
-        '''
-        :param name: Name of Jenkins node, ``str``
-        :returns: ``True`` if Jenkins node exists
-        '''
-        try:
-            self.get_node_info(name)
-            return True
-        except JenkinsException:
-            return False
