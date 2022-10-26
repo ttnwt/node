d0b59ee109ca07e5c9c3c967d275ace12cdd6735
ttnwt <103345581+ttnwt@users.noreply.github.com>
2022-10-25 23:17:21 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,23 +1,5 @@
 print("Hello World")
-import hudson.model.*
-import hudson.plugins.ec2.*
-
-def printInstanceInfo = { Slave item ->
-
-  computer = item.computer
-
-  if(item instanceof EC2AbstractSlave) {
-    i = computer.describeInstance()
-
-    println computer.name.padRight(30).replaceAll("\\(.*\\)","") + " " + i.getPrivateIpAddress().padRight(15) + " ${i.getInstanceId()} ${i.getImageId()} " + i.getInstanceType().padLeft(10) +" ${i.getLaunchTime().format('YYYY-MM-dd HH:mm')}" +  (computer.offline ? " (OFFLINE)" : "")
-  } else {
-    println computer.name.padLeft(30)+" ${computer.hostName}"  + (computer.offline ? " (OFFLINE)" : "")
-  }
-
+def labels = jenkins.getLabels()
+labels.each {
+   println "${it.displayName}"
 }
-
-
-Hudson.instance.slaves.each { printInstanceInfo(it) }
-
-return
-
